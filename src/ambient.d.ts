cad45034f7dd0dd40f36d10d592075302ea229b0
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:11 -0500

    new comment

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,4 +1,14 @@
+// This is a declare module syntax in TypeScript which allows us to create a declaration for a module that may be written in a different language or located in a different file.
+
 declare module "*.ttf" {
-	const value: ArrayBuffer;
-	export default value;
+  // The module being declared here is for TypeScript files that import TrueType Font (TTF) files.
+  // The '*.ttf' pattern is used to specify that this declaration applies to all TTF files.
+
+  // The 'value' constant is defined as an ArrayBuffer type.
+  // ArrayBuffer is a standard, low-level, typeless binary data buffer built into the web platform.
+  const value: ArrayBuffer;
+
+  // The 'export default' syntax exports the 'value' constant as the default export of this module.
+  // This means that when a TypeScript file imports a TTF file using the 'import' keyword, it will receive the ArrayBuffer 'value' as the default import.
+  export default value;
 }
