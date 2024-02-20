ad87d95997f5be06945827e4c0b93b5ca5dfce49
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:17 -0500

    new comment

--- src/lib/types/User.ts
+++ src/lib/types/User.ts
@@ -1,12 +1,26 @@
+// Import the 'ObjectId' type from the 'mongodb' package
 import type { ObjectId } from "mongodb";
+
+// Import the 'Timestamps' type from the './Timestamps' module
 import type { Timestamps } from "./Timestamps";
 
+// Export the 'User' interface which extends the 'Timestamps' interface
 export interface User extends Timestamps {
-	_id: ObjectId;
+    // The 'id' property is required and should be of type 'ObjectId'
+    id: ObjectId;
 
-	username?: string;
-	name: string;
-	email?: string;
-	avatarUrl: string | undefined;
-	hfUserId: string;
+    // The 'username' property is optional and should be of type 'string'
+    username?: string;
+
+    // The 'name' property is required and should be of type 'string'
+    name: string;
+
+    // The 'email' property is optional and should be of type 'string'
+    email?: string;
+
+    // The 'avatarUrl' property is required and should be of type 'string' or 'undefined'
+    avatarUrl: string | undefined;
+
+    // The 'hfUserId' property is required and should be of type 'string'
+    hfUserId: string;
 }
