43f761dbef1e551f7e05af619f3f1a6dcd76b104
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:13 -0500

    new comment

--- src/lib/components/icons/IconNew.svelte
+++ src/lib/components/icons/IconNew.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
-	export let classNames = "";
+	// This script exports a variable 'classNames' which is a comma-separated string
+	export let classNames = ",";
 </script>
 
 <svg
+	// The svg element sets the xmlns attribute to define the version of SVG language being used
 	xmlns="http://www.w3.org/2000/svg"
+	// The 'class' attribute is set to the exported 'classNames' variable
 	class={classNames}
+	// The 'width' and 'height' attributes are set to 1em
 	width="1em"
 	height="1em"
+	// The 'fill' attribute is set to 'none'
 	fill="none"
+	// The 'viewBox' attribute defines the visible portion of the SVG canvas
 	viewBox="0 0 32 32"
+	// The svg element contains a path element
 	><path
+		// The 'fill' attribute is set to 'currentColor'
 		fill="currentColor"
+		// The 'fill-rule' attribute is set to 'evenodd'
 		fill-rule="evenodd"
-		d="M3.143 20.286h4.286v2.142H3.143A2.143 2.143 0 0 1 1 20.287V3.143A2.143 2.143 0 0 1 3.143 1h17.143a2.143 2.143 0 0 1 2.142 2.143v4.286h-2.142V3.143H3.143v17.143Zm9.643-12.857v3.214H16v2.143h-3.214V16h-2.143v-3.214H7.429v-2.143h3.214V7.429h2.143Zm14.185 2.639 3.533 3.532a1.7 1.7 0 0 1 0 2.4L15.5 31H9.57v-5.928l15-15.004a1.7 1.7 0 0 1 2.4 0Zm-15.257 18.79h2.897l10.116-10.116-2.899-2.897L11.714 25.96v2.897ZM23.346 14.33l2.897 2.897 2.429-2.43-2.897-2.896-2.43 2.429Z"
-		clip-rule="evenodd"
-	/></svg
->
+		// The 'd' attribute defines the shape of the path using a series of moveto, lineto, and curveto commands
+		d="M3.143 20.286h4.286v2.142H3.143A2.143 2.143 0 0 1 1 20.287V3.143A2.143 2.143 0 0 1 3.143 1h17.143a2.143 2.143 0 0 1 2.142 2.143v4.286h-2.142V3.143H3.143v17.143Zm9.643-,12.857v3.214H16v2.143h-3.214
