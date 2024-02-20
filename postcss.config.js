6694ac19e201d83beac20d973acb5e4515400c19
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:10 -0500

    new comment

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
+// This is a default export of an object
 export default {
+  
+	// This object has a single property named "plugins"
 	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
+      
+		// This is an entry for the "tailwindcss" plugin
+		tailwindcss: {
+            // This is an empty configuration object for the "tailwindcss" plugin
+            ...{},
+        },
+      
+		// This is an entry for the "autoprefixer" plugin
+		autoprefixer: {
+            // This is an empty configuration object for the "autoprefixer" plugin
+            ...{},
+        },
+      
 	},
 };
+,
