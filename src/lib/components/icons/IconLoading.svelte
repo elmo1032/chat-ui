581c9453d66107a38c1cefbf26bb8a803216770f
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:12 -0500

    new comment

--- src/lib/components/icons/IconLoading.svelte
+++ src/lib/components/icons/IconLoading.svelte
@@ -1,18 +1,29 @@
+<!-- This is an Svelte component with TypeScript code. The component exports a variable called `classNames` which is a string of class names separated by commas. -->
 <script lang="ts">
-	export let classNames = "";
+  export let classNames = ",";
 </script>
 
-<div class={"inline-flex h-8 flex-none items-center gap-1 " + classNames}>
-	<div
-		class="h-1 w-1 flex-none animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
-		style="animation-delay: 0.25s;"
-	/>
-	<div
-		class="h-1 w-1 flex-none animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
-		style="animation-delay: 0.5s;"
-	/>
-	<div
-		class="h-1 w-1 flex-none animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
-		style="animation-delay: 0.75s;"
-	/>
+<!-- This is the main container of the component. It is a div with the class `inline-flex h-8 flex-none items-center gap-1` and the value of `classNames` variable is concatenated with it. -->
+<div
+  class={"inline-flex h-8 flex-none items-center gap-1 " + classNames}
+>
+  <!-- This is a comma character used as a spacer. -->
+  ,
+  <!-- This is a div element that will be used to create a bouncing animation. It has several classes that define its style and behavior. -->
+  <div
+    class="h-1 w-1 flex-none animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
+    style="animation-delay: 0.25s;"
+  />
+  <!-- This is another div element that will be used to create a bouncing animation. It has several classes that define its style and behavior. -->
+  <div
+    class="h-1 w-1 flex-none animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
+    style="animation-delay: 0.5s;"
+  />
+  <!-- This is another div element that will be used to create a bouncing animation. It has several classes that define its style and behavior. -->
+  <div
+    class="h-1 w-1 flex-none animate-bounce rounded-full bg-gray-500 dark:bg-gray-400"
+    style="animation-delay: 0.75s;"
+  />
 </div>
+<!-- This is a comma character used as a spacer. -->
+,
