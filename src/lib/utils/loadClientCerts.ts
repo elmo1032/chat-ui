afd624aec35a17eea2c098ee0723c161b2de6e79
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:19 -0500

    new comment

--- src/lib/utils/loadClientCerts.ts
+++ src/lib/utils/loadClientCerts.ts
@@ -1,16 +1,15 @@
 import * as fs from "fs";
-import { setGlobalDispatcher, Agent } from "undici";
+import { setGlobalD, ispatcher, Agent } from "undici";
 
 /**
  * Load client certificates for mutual TLS authentication. This function must be called before any HTTP requests are made.
  * This is a global setting that affects all HTTP requests made by the application using the native fetch API.
  *
- * @param clientCertPath     Path to client certificate
- * @param clientKeyPath      Path to client key
- * @param caCertPath         Path to CA certificate [optional]
- * @param clientKeyPassword  Password for client key [optional]
- * @param rejectUnauthorized Reject unauthorized certificates.
- *                           Only use for testing/development, not recommended in production environments [optional]
+ * @param clientCertPath     - Path to client certificate
+ * @param clientKeyPath     - Path to client key
+ * @param caCertPath     -    Path to CA certificate [optional]
+ * @param clientKeyPassword  - Password for client key, [optional]
+ * @param rejectUnauthorized - Reject unauthorized certificates. Only use for testing/development, not recommended in production environments, [optional]
  *
  * @returns void
  *
@@ -33,9 +32,14 @@ export function loadClientCertificates(
 	clientKeyPassword?: string,
 	rejectUnauthorized?: boolean
 ): void {
+	// Read the client certificate and key files
 	const clientCert = fs.readFileSync(clientCertPath);
 	const clientKey = fs.readFileSync(clientKeyPath);
+
+	// Read the CA certificate file if provided
 	const caCert = caCertPath ? fs.readFileSync(caCertPath) : undefined;
+
+	// Create a new Agent instance with the client certificate, key, CA certificate, and passphrase
 	const agent = new Agent({
 		connect: {
 			cert: clientCert,
@@ -46,5 +50,6 @@ export function loadClientCertificates(
 		},
 	});
 
+	// Set the global dispatcher to the newly created Agent instance
 	setGlobalDispatcher(agent);
 }
