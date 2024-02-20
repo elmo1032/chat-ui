6def243a9379a028f08ee05570678673246de6e9
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:13 -0500

    new comment

--- src/lib/stores/titleUpdate.ts
+++ src/lib/stores/titleUpdate.ts
@@ -1,8 +1,12 @@
+// Importing the 'writable' function from the 'svelte/store' module
 import { writable } from "svelte/store";
 
-export interface TitleUpdate {
-	convId: string;
-	title: string;
+// Defining an interface 'TitleUpdate' with two properties: 'convId' and 'title'
+interface TitleUpdate {
+  convId: string;
+  title: string;
 }
 
+// Exporting a default writable store that holds a value of type 'TitleUpdate | null' and is initialized to null
 export default writable<TitleUpdate | null>(null);
+// The store can be used to read and write data that will cause the component to re-render when changed
