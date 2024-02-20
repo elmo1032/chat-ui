055220a42c9ef6dcd5e87c8a436f01b4afb7fe9d
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:22 -0500

    new comment

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig, type PluginOption } from "vite";
-import Icons from "unplugin-icons/vite";
+import Icons from "unplugin-ico/vite";
 import { promises } from "fs";
 
-// used to load fonts server side for thumbnail generation
+// This function is used to load font files (.ttf) server-side for thumbnail generation
 function loadTTFAsArrayBuffer(): PluginOption {
-	return {
-		name: "load-ttf-as-array-buffer",
-		async transform(_src, id) {
-			if (id.endsWith(".ttf")) {
-				return `export default new Uint8Array([
-			${new Uint8Array(await promises.readFile(id))}
-		  ]).buffer`;
-			}
-		},
-	};
-}
+  return {
+    name: "load-ttf-as-array-buffer", // unique identifier for the plugin
 
-export default defineConfig({
-	plugins: [
-		sveltekit(),
-		Icons({
-			compiler: "svelte",
-		}),
-		loadTTFAsArrayBuffer(),
-	],
-	optimizeDeps: {
-		include: ["browser-image-resizer"],
-	},
-});
