8f3e68b806bd6f5d25fa8103af8f80cbd52bb4b7
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:10 -0500

    new comment

--- scripts/updateLocalEnv.ts
+++ scripts/updateLocalEnv.ts
@@ -1,20 +1,23 @@
+// Import the 'fs' module for file system operations
 import fs from "fs";
 
+// Check if the file '.env.SECRET\_CONFIG' exists, and if it does, read its contents
 const SECRET_CONFIG = fs.existsSync(".env.SECRET_CONFIG")
-	? fs.readFileSync(".env.SECRET_CONFIG", "utf8")
-	: process.env.SECRET_CONFIG;
+  ? fs.readFileSync(".env.SECRET_CONFIG", "utf8")
+  : process.env.SECRET_CONFIG;
 
+// If SECRET\_CONFIG is not defined, throw an error
 if (!SECRET_CONFIG) {
-	throw new Error(
-		"SECRET_CONFIG is not defined. Please provide it either in a file or as an environment variable."
-	);
+  throw new Error(
+    "SECRET\_CONFIG is not defined. Please provide it either in a file or as an environment variable."
+  );
 }
 
-// Read the content of the file .env.template
+// Read the contents of the file '.env.template'
 const PUBLIC_CONFIG = fs.readFileSync(".env.template", "utf8");
 
-// Prepend the content of the env variable SECRET_CONFIG
-const full_config = `${PUBLIC_CONFIG}\n${SECRET_CONFIG}`;
+// Prepend the contents of the SECRET\_CONFIG environment variable to PUBLIC\_CONFIG
+const full_config = `\n${SECRET_CONFIG}`;
 
-// Write full_config to .env.local
+// Write full\_config to '.env.local'
 fs.writeFileSync(".env.local", full_config);
