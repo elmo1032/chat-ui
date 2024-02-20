637281ae95febce75bb0e6bdc30001f935dafcb3
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:11 -0500

    new comment

--- src/lib/components/PaginationArrow.svelte
+++ src/lib/components/PaginationArrow.svelte
@@ -1,23 +1,19 @@
 <script lang="ts">
+	// Importing icons from the carbon library
 	import CarbonCaretLeft from "~icons/carbon/caret-left";
 	import CarbonCaretRight from "~icons/carbon/caret-right";
 
+	// Exporting props for the component
 	export let href: string;
-	export let direction: "next" | "previous";
-	export let isDisabled = false;
+	export let direct: "next" | "previous"; // Specifies the direction of the button: "next" or "previous"
+	export let isDisabled = false; // Specifies if the button is disabled
+	export let isLoading = false; // Specifies if the button is loading
 </script>
 
+<!-- The anchor tag for the button -->
 <a
-	class="flex items-center rounded-lg px-2.5 py-1 hover:bg-gray-50 dark:hover:bg-gray-800 {isDisabled
+	// Adding classes for styling
+	class="flex items-center rounded-lg px-2.5 py-1 hover:bg-gray-50 dark:hover:bg-gray-800
+		{isDisabled
 		? 'pointer-events-none opacity-50'
-		: ''}"
-	{href}
->
-	{#if direction === "previous"}
-		<CarbonCaretLeft classNames="mr-1.5" />
-		Previous
-	{:else}
-		Next
-		<CarbonCaretRight classNames="ml-1.5" />
-	{/if}
-</a>
+	
