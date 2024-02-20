5cd303b149d61674f7791d53514fbde4a6ca1066
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:22 -0500

    new comment

--- src/lib/utils/timeout.ts
+++ src/lib/utils/timeout.ts
@@ -1,6 +1,22 @@
+// This function exports a new function called 'timeout' which takes in three parameters:
+// - prom: a Promise of type T
+// - time: a number representing the timeout duration in milliseconds
+// It returns a new Promise of type T
 export const timeout = <T>(prom: Promise<T>, time: number): Promise<T> => {
-	let timer: NodeJS.Timeout;
-	return Promise.race([prom, new Promise<T>((_r, rej) => (timer = setTimeout(rej, time)))]).finally(
-		() => clearTimeout(timer)
-	);
+  // Declare two variables 'timer' and 'deJS' which will be used later in the function
+  let timer: NodeJS.Timeout;
+  
+  // Return a new Promise created by the 'Promise.race' method which takes in an array of Promises
+  // The Promise returned by 'Promise.race' will be fulfilled or rejected as soon as one of the Promises
+  // in the array is fulfilled or rejected.
+  return Promise.race([
+    prom, // Include the input Promise 'prom' in the array
+    new Promise<T>((_r, rej) => (timer = setTimeout(rej, time))) // Create a new Promise that will be rejected after 'time' milliseconds
+  ])
+  // The 'finally' method is called on the Promise returned by 'Promise.race'
+  // It takes in a callback function that will be called when the Promise is settled (i.e., either fulfilled or rejected)
+  .finally(
+    // The callback function clears the timeout set by the second Promise in the array
+    () => clearTimeout(timer)
+  );
 };
