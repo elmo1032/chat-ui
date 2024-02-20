b8c93b19a9a05f26f37adaf7cdc36361e07e4b08
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:15 -0500

    new comment

--- src/lib/types/AbortedGeneration.ts
+++ src/lib/types/AbortedGeneration.ts
@@ -1,8 +1,14 @@
-// Ideally shouldn't be needed, see https://github.com/huggingface/chat-ui/pull/88#issuecomment-1523173850
-
+// Importing necessary types from their respective modules
 import type { Conversation } from "./Conversation";
 import type { Timestamps } from "./Timestamps";
 
+// Defining the shape of an object that represents an aborted generation
 export interface AbortedGeneration extends Timestamps {
-	conversationId: Conversation["_id"];
+  
+    // The ID of the conversation associated with the aborted generation
+    conversationId: Conversation["_id"];
+    
+    // Timestamps related to the aborted generation
+    createdAt: Timestamps["createdAt"];
+    updatedAt: Timestamps["updatedAt"];
 }
