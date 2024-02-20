c85bdae86c696b067d811b0cc30c4fd7031ae134
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:19 -0500

    new comment

--- src/routes/r/[id]/+page.ts
+++ src/routes/r/[id]/+page.ts
@@ -1,5 +1,7 @@
+// Import the 'redirect' function from the '@sveltejs/kit' module
 import { redirect } from "@sveltejs/kit";
 
+// Define the 'load' function, which is an asynchronous function that takes an object 'params' as an argument
 export const load = async ({ params }) => {
-	throw redirect(302, "../conversation/" + params.id);
-};
+  // Redirect the user to the URL "/conversation/{params.id}" with a 302 status code
+  throw redirect(3
