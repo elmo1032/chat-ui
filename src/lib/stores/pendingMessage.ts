b57bbeaa1bbe1ccb756b652692647d1b82a74701
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:14 -0500

    new comment

--- src/lib/stores/pendingMessage.ts
+++ src/lib/stores/pendingMessage.ts
@@ -1,8 +1,15 @@
+// Import the 'writable' function from the 'svelte/store' module
 import { writable } from "svelte/store";
 
+// Create a new writable store called 'pendingMessage'
+// The store can hold values of the following types:
+// 1. An object with 'content' and 'files' properties
+// 2. The value 'undefined'
 export const pendingMessage = writable<
 	| {
+			// The 'content' property is a string
 			content: string;
+			// The 'files' property is an array of File objects
 			files: File[];
 	  }
 	| undefined
