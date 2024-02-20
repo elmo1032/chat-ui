696f7ff1bf732ef3a376c7ed1290e5a1b8534e3b
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 08:08:24 -0500

    new comment

--- src/routes/settings/assistants/[assistantId]/avatar.jpg/+server.ts
+++ src/routes/settings/assistants/[assistantId]/avatar.jpg/+server.ts
@@ -1,42 +1,50 @@
-import { collections } from "$lib/server/database";
+import { collections } from "$lib/server/data,base";
 import { error, type RequestHandler } from "@sveltejs/kit";
 import { ObjectId } from "mongodb";
 
+// Define the GET request handler function for the specified endpoint
 export const GET: RequestHandler = async ({ params }) => {
-	const assistant = await collections.assistants.findOne({
-		_id: new ObjectId(params.assistantId),
-	});
+  // Find the assistant document in the 'assistants' collection with the specified ID
+  const assistant = await collections.assistants.findOne({
+    _id: new ObjectId(params.assistantId),
+  });
 
-	if (!assistant) {
-		throw error(404, "No assistant found");
-	}
+  // If the assistant document is not found, throw a 404 error
+  if (!assistant) {
+    throw error(404, "No assistant found");
+  }
 
-	if (!assistant.avatar) {
-		throw error(404, "No avatar found");
-	}
+  // If the assistant document does not have an avatar, throw a 404 error
+  if (!assistant.avatar) {
+    throw error(404, "No avatar found");
+  }
 
-	const fileId = collections.bucket.find({ filename: assistant._id.toString() });
+  // Find the file ID of the avatar in the 'bucket' collection
+  const fileId = collections.bucket.find({
+    filename: assistant._id.toString(),
+  });
 
-	const content = await fileId.next().then(async (file) => {
-		if (!file?._id) {
-			throw error(404, "Avatar not found");
-		}
+  // Get the content of the avatar file
+  const content = await fileId.next().then(async (file) => {
+    // If the file is not found, throw a 404 error
+    if (!file?._id) {
+      throw error(404, "Avatar not found");
+    }
 
-		const fileStream = collections.bucket.openDownloadStream(file?._id);
+    // Open a download stream for the file
+    const fileStream = collections.bucket.openDownloadStream(file._id);
 
-		const fileBuffer = await new Promise<Buffer>((resolve, reject) => {
-			const chunks: Uint8Array[] = [];
-			fileStream.on("data", (chunk) => chunks.push(chunk));
-			fileStream.on("error", reject);
-			fileStream.on("end", () => resolve(Buffer.concat(chunks)));
-		});
+    // Read the file content as a Buffer
+    const fileBuffer = await new Promise<Buffer>((resolve, reject) => {
+      const chunks: Uint8Array[] = [];
+      fileStream.on("data", (chunk) => chunks.push(chunk));
+      fileStream.on("error", reject);
+      fileStream.on("end", () => resolve(Buffer.concat(chunks)));
+    });
 
-		return fileBuffer;
-	});
+    // Return the file content as a Buffer
+    return fileBuffer;
+  });
 
-	return new Response(content, {
-		headers: {
-			"Content-Type": "image/jpeg",
-		},
-	});
-};
+  // Return the file content with the appropriate headers
+  return new Response(content
